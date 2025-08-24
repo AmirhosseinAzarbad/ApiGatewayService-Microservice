8b355e718f10bbc435a2e06c12842b3160de478b
speedy <thenewamirech@gmail.com>
2025-08-24 08:24:48 +0330

    Initial commit with dependencies

---
+++ src/test/java/ir/speedy/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.speedy.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
